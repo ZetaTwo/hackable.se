e868e597680fb716d1f06eba3a2ae133ff2a2527
Calle Svensson <calle.svensson@zeta-two.com>
2020-07-02 09:15:51 -0700

    Created more tables

--- api/src/schema.rs
+++ api/src/schema.rs
@@ -1,4 +1,41 @@
 table! {
+    challenge_tags (id) {
+        id -> Binary,
+        challenge -> Binary,
+        tag -> Binary,
+        created -> Datetime,
+    }
+}
+
+table! {
+    challenges (id) {
+        id -> Binary,
+        name -> Varchar,
+        description -> Text,
+        created -> Datetime,
+    }
+}
+
+table! {
+    sessions (id) {
+        id -> Binary,
+        token -> Nullable<Binary>,
+        user -> Nullable<Binary>,
+        expires -> Datetime,
+        created -> Datetime,
+    }
+}
+
+table! {
+    tags (id) {
+        id -> Binary,
+        name -> Varchar,
+        description -> Text,
+        created -> Datetime,
+    }
+}
+
+table! {
     users (id) {
         id -> Binary,
         username -> Varchar,
@@ -7,3 +44,11 @@ table! {
         email_validated -> Bool,
     }
 }
+
+allow_tables_to_appear_in_same_query!(
+    challenge_tags,
+    challenges,
+    sessions,
+    tags,
+    users,
+);
